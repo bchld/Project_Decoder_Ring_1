d86b1fd52c988ed0f21fe087d025ca295978eeb3
bchld <88337291+bchld@users.noreply.github.com>
2021-09-13 22:06:45 -0500

    Add files via upload
    Completed in webide

--- Project_Decoder_Ring_1/src/caesar.js
+++ Project_Decoder_Ring_1/src/caesar.js
@@ -8,6 +8,28 @@ const caesarModule = (function () {
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+      //statement to return false if shift parameters are missing or to large/small
+      if(!shift || shift > 25 || shift < -25) return false;
+    
+      //create inverse shift code for decoding
+      let decode = Math.abs(shift) * -1
+      
+     // make sure input is all lower case
+      let lowerInput = input.toLowerCase();
+      
+    
+   if(encode === true && shift > 0) {
+     // return shifted message for encoded cipher
+     return lowerInput.replace(/[a-z]/g, c => String.fromCharCode((c.charCodeAt(0)-97 + shift) % 26 + 97))
+     //return decoded message if encode = false
+     
+   }else if(encode === false && shift < 0){
+   return lowerInput.replace(/[a-z]/g, c => String.fromCharCode((c.charCodeAt(0)-97 + shift*-1) % 26 + 97))
+   
+   } else {return lowerInput.replace(/[a-z]/g, c => String.fromCharCode((c.charCodeAt(0)-122 + decode) % 26 + 122)) 
+          };
+           
+    //
   }
 
   return {
